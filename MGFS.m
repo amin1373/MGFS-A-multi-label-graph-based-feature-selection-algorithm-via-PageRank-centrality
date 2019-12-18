f4faaa707ae0708e66f5e69502d575abd7319db9
amin1373 <59017215+amin1373@users.noreply.github.com>
2019-12-18 19:39:42 +0330

    Update MGFS.m

--- MGFS.m
+++ MGFS.m
@@ -1,4 +1,4 @@
-function Frank = MGFS(X_train, Y_train)
+function FRank = MGFS(X_train, Y_train)
 cordis = pdist2(X_train',Y_train','correlation');
 cordis(isnan(cordis)) = 0;
 Edg = pdist2(cordis,cordis);
